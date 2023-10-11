52e787004f5fdd758acfd43addbc8a3969c074e4
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-10-12 04:24:36 +0900

    :lipstick: 아이템 카드 배치 및 스타일 변경

--- src/routes/restaurants/+page.svelte
+++ src/routes/restaurants/+page.svelte
@@ -5,36 +5,50 @@
 
 <h1>아보하 맛집 리스트</h1>
 
-{#each database as { restaurant, location, writer, created }}
-	<div class="card">
-		<span class="restaurant">{restaurant}</span>
-		<span class="location">{location}</span>
-		<div class="created-info">
-			<span class="created">{created}</span>
-			<span class="writer">{writer}</span>
+<div class="container">
+	{#each database as { restaurant, location, writer, created, type }}
+		<div class="card">
+			<span class="restaurant">{restaurant}</span>
+			<span class="location">{location}</span>
+			<span class="type">{type}</span>
+			<div class="created-info">
+				<span class="created">{created}</span>
+				<span class="writer">{writer}</span>
+			</div>
 		</div>
-	</div>
-{/each}
+	{/each}
+</div>
 
 <style>
+	.container {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-evenly;
+	}
 	.card {
-		background-color: lightsalmon;
+		background-color: rgb(206, 234, 223);
 		border-radius: 10px;
 		padding: 15px;
-		width: 260px;
+		width: 240px;
+		height: 120px;
 		margin: 20px;
+		position: relative;
 	}
 	.restaurant {
 		display: block;
 		font-size: 20px;
 		margin-bottom: 5px;
 	}
-	.location {
+	.location,
+	.type {
 		display: block;
 		font-size: 15px;
 	}
 	.created-info {
 		font-size: 13px;
 		text-align: right;
+		position: absolute;
+		bottom: 15px;
+		right: 15px;
 	}
 </style>
