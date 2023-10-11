010a467beb980d498dbbc4e1d7ee6141d89d1a4f
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-10-12 04:17:24 +0900

    :lipstick: 아이템 카드 스타일 변경

--- src/routes/restaurants/+page.svelte
+++ src/routes/restaurants/+page.svelte
@@ -5,19 +5,36 @@
 
 <h1>아보하 맛집 리스트</h1>
 
-{#each database as d}
+{#each database as { restaurant, location, writer, created }}
 	<div class="card">
-		<p>작성자: {d.writer}</p>
-		<p>작성일: {d.created}</p>
-		<p>식당: {d.restaurant}</p>
-		<p>위치: {d.location}</p>
+		<span class="restaurant">{restaurant}</span>
+		<span class="location">{location}</span>
+		<div class="created-info">
+			<span class="created">{created}</span>
+			<span class="writer">{writer}</span>
+		</div>
 	</div>
 {/each}
 
 <style>
 	.card {
 		background-color: lightsalmon;
-		border-radius: 20px;
+		border-radius: 10px;
 		padding: 15px;
+		width: 260px;
+		margin: 20px;
+	}
+	.restaurant {
+		display: block;
+		font-size: 20px;
+		margin-bottom: 5px;
+	}
+	.location {
+		display: block;
+		font-size: 15px;
+	}
+	.created-info {
+		font-size: 13px;
+		text-align: right;
 	}
 </style>
