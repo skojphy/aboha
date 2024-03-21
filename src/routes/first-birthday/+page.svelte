90b687f36402bb501b99d8b44af7432b44d625f1
서혜연 <seohyeyeon@seohyeyeon-ui-MacBookAir.local>
2024-03-21 11:40:55 +0900

    🎉 아보하돌 페이지 생성

---
+++ src/routes/first-birthday/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	// export let data;
+	import mainImage from '$lib/dol/images/invitation1.png';
+
+</script>
+
+<header>
+	<h1>아보하돌</h1>
+	<img src={mainImage} alt="아보하, 첫 돌. 소중한 아기의 첫 번째 생일을 축하해 주세요.">
+</header>
+
+<div class="container">
+아.보.하.돌.
+</div>
+
+<style>
+
+</style>
