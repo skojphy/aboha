569e279c6b8efcf6c77beeb92b47423b95d87b95
서혜연 <seohyeyeon@seohyeyeon-ui-MacBookAir.local>
2024-03-23 01:54:23 +0900

    ✨ 메인 페이지 수정

--- src/routes/first-birthday/+page.svelte
+++ src/routes/first-birthday/+page.svelte
@@ -1,18 +1,30 @@
 <script>
 	// export let data;
 	import mainImage from '$lib/dol/images/invitation1.png';
-
 </script>
 
 <header>
-	<h1>아보하돌</h1>
-	<img src={mainImage} alt="아보하, 첫 돌. 소중한 아기의 첫 번째 생일을 축하해 주세요.">
+	<h1 class="visually-hidden">아보하 돌</h1>
 </header>
+<main class="container">
+	<img
+		src={mainImage}
+		alt="아보하, 첫 돌. 소중한 아기의 첫 번째 생일을 축하해 주세요."
+		class="main-image"
+	/>
 
-<div class="container">
-아.보.하.돌.
-</div>
+	<div>아.보.하.돌.</div>
+</main>
+
+<footer>footer</footer>
 
 <style>
+	main {
+		padding: 0;
+	}
 
+	.main-image {
+		width: 100vw;
+		max-width: 480px;
+	}
 </style>
